a502c5a817408e2997747fadac93552adf54607d
Mujahid Ali <mujahidali7869211@gmail.com>
2021-10-24 18:40:11 +0500

    task done

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import './App.css';
+import React from 'react';
+import Spinner from '../src/Components/Spinner'
+function App() {
+  return (
+    <div className="App">
+      <Spinner/>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/Components/Spinner.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import '../App.css';
+
+
+function spinner() {
+    return (
+        <div>
+            <div className="spinner">
+            </div>
+        </div>
+    )
+}
+
+export default spinner
